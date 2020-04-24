34c9843fb4abe99dff0347372b122e9821590d15
“Sourav <duttasourav029@gmail>
2020-04-24 22:31:30 +0530

    Actions and payload

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -7,7 +7,7 @@
         <span class="price"> INR {{product.price}}</span>
       </li>
     </ul>  
-    <button v-on:click="reducePrice">Reduce Price</button>  
+    <button v-on:click="reducePrice(4)">Reduce Price</button>  
   </div>
 </template>
 
@@ -23,11 +23,11 @@ export default {
     }
   },
   methods:{
-    reducePrice: function(){
+    reducePrice: function(amount){
       // this.$store.state.products.forEach(product=>{
       //   product.price -= 1
       //})
-      this.$store.commit('reducePrice');
+      this.$store.dispatch('reducePrice', amount);
     }
   }
 }
