f832d70742344a63b5bef913881dd9b533bffc18
“Sourav <duttasourav029@gmail>
2020-04-24 21:28:15 +0530

    installation of vueapp and vuex

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
 
 export default {
+  components:{
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo,
+  },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      
+
+    }
+  },
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    font-family: Ubuntu;
+    color: #555;
+  }
 </style>
