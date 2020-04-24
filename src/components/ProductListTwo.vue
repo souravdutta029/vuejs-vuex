7a115940b167d3b138aa5ab2f98c08de4999e81b
“Sourav <duttasourav029@gmail>
2020-04-24 21:52:34 +0530

    getters

--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,7 +2,7 @@
   <div id="product-list-two">
     <h2>Product List Two</h2>
     <ul>
-        <li v-for="product in products" v-bind:key="product">
+        <li v-for="product in saleProducts" v-bind:key="product">
         <span class="name">{{product.name}}</span>
         <span class="price">INR {{product.price}}</span>
       </li>
@@ -16,6 +16,9 @@ export default {
   computed:{
     products(){
       return this.$store.state.products;
+    },
+    saleProducts(){
+      return this.$store.getters.saleProducts;
     }
   }
 }
